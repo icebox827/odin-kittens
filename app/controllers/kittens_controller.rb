7fc238bed1b442fa2fb78f865dec5facefd0d403
Denis Lafontant <icebox827@gmail.com>
2021-05-07 16:22:06 -0400

    Update Kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,29 @@
 class KittensController < ApplicationController
   def index
-    @kitten = Kitten.all
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kittens_params)
+    if @kitten.save
+      flash[:notice] = 'Kitten created successfully'
+      redirect_to root_path(@kitten)
+    else
+      flash[:alert] = 'Kitten not created, please try again'
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
