86e0c41ba8da405954185427eae6b57988261d6b
Denis Lafontant <icebox827@gmail.com>
2021-05-07 17:35:35 -0400

    Update CRUD method for kitten

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -18,12 +18,28 @@ class KittensController < ApplicationController
       redirect_to root_path(@kitten)
     else
       flash[:alert] = 'Kitten not created, please try again'
+      render 'new'
     end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update(name: params[:kitten][:name], age: params[:kitten][:age])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
   end
 
   private
 
   def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    redirect_to kitten_path(@kitten)
   end
 end
