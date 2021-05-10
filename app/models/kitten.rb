00b5a9bfc0a5c0db1f234846c199748904bb5b84
Denis Lafontant <icebox827@gmail.com>
2021-05-10 10:23:38 -0400

    Update model with new validation

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,4 +1,4 @@
 class Kitten < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: true }, length: { in: 3..20 }
-  validates :age, presence: true
+  validates :age, :cuteness, :softness, presence: true
 end
